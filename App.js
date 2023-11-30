13cd96548cffcd5efe2f9e609fb579dbecfefcec
Terence Faid JABO <faidterence@outlook.com>
2023-11-30 21:54:27 +0200

    Add Navigation to WeatherApp

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native';
 import CurrentWeather from './src/screens/CurrentWeather';
-import UpCommingWeather from './src/screens/UpCommingWeather';
 import City from './src/screens/City';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import UpCommingWeather from './src/screens/UpCommingWeather';
+import { Feather } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-      {/* <UpCommingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen
+          name="CurrentWeather"
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="droplet"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="UpComingWeather"
+          component={UpCommingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="clock"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="City"
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="home"
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-});
+
 export default App;
