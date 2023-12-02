4970fd7af794747db85ef43fb55fe4a363ca0679
Terence Faid JABO <faidterence@outlook.com>
2023-12-02 16:00:59 +0200

    feat(App): Added loading status

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
-import Counter from './src/Demo/Counter';
-
+import * as Location from 'expo-location';
+import {API_URL} from '@env';
 const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  console.log(API_URL)
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('permission to access location was denied');
+        return;
+      }
+      let { location } = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (location) {
+    console.log(location);
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      {/* <Tabs /> */}
-      <Counter />
+      <Tabs />
+      {/* <Counter /> */}
     </NavigationContainer>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
--- src/Demo/Counter.js
+++ src/Demo/Counter.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, View, Text } from 'react-native';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+  useEffect(() => {
+    console.log(`Our Cont value is ${count}`);
+    return () => {
+      console.log('Use effect cleanup');
+    };
+  }, [count]);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
+      <Text style={styles.title}>{newCount}</Text>
       <Button
         color={'red'}
         title="Increment the Count"
@@ -18,6 +26,20 @@ const Counter = () => {
         title="Decrement the count"
         onPress={() => {
           setCount(count - 1);
+        }}
+      />
+      <Button
+        color={'red'}
+        title="Increment the Count"
+        onPress={() => {
+          setNewCount(newCount + 1);
+        }}
+      />
+      <Button
+        color={'green'}
+        title="Decrement the count"
+        onPress={() => {
+          setNewCount(count - 1);
         }}
       />
     </View>
